877bfdd579387ec3a95a308731ef27b6b35d5072
sindooja044 <aaeer015@gmail.com>
2026-05-12 22:37:45 +0530

    add middleware protected route

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,17 +1,18 @@
 const {add,getTask, toggleTask,deleteTask,updateTask}=require("../controllers/taskController");
 const {userRegister,userLogin}=require("../controllers/userController");
+const protect = require('../middleware/authMiddleware');
 
 const express=require('express')
 const router=express.Router();
 
 
 
-router.post("/", add)
+router.post("/",protect,add)
 
-router.get("/", getTask)
-router.patch("/:id", toggleTask)
-router.delete("/:id", deleteTask)
-router.put("/:id", updateTask)
+router.get("/", protect, getTask)
+router.patch("/:id",protect,toggleTask)
+router.delete("/:id",protect, deleteTask)
+router.put("/:id",protect, updateTask)
 router.post('/register', userRegister)
 router.post('/login', userLogin)
 module.exports=router
